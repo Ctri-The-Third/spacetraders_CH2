9c86bd9597d642a695e7dd69f7ea9d86b9cd1e90
C'tri Goudie <CtriCoding@Ctri.co.uk>
2024-03-02 19:34:28 +0000

    list all possible trades and have single-click execution

--- scripts/PRD-build_and_run_everything.bat
+++
@@ -1,4 +0,0 @@
-docker build  -f scripts\.dockerfile -t ctriatanitan/spacetraders_ch2_ui  .
-docker stop spacetraders_ch2_ui
-docker rm spacetraders_ch2_ui
-docker run -d -p 3000:3000 --name spacetraders_ch2_ui --env-file scripts/.env ctriatanitan/spacetraders_ch2_ui 
